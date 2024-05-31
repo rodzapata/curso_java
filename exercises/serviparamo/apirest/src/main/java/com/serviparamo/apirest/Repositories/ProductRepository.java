2270126f2278b66acc41df6d77a9e911c00f66dd
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-05-31 09:47:14 -0500

    delete proyect serviparamo

--- exercises/serviparamo/apirest/src/main/java/com/serviparamo/apirest/Repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.serviparamo.apirest.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.serviparamo.apirest.Entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-
-}
